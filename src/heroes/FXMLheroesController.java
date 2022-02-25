bc788b4155e88b56d171c919bf1f27b48c87d506
KennethVilla <100308559+KennethVilla@users.noreply.github.com>
2022-02-24 20:59:48 -0500

    primer commit

--- src/heroes/FXMLheroesController.java
+++ src/heroes/FXMLheroesController.java
@@ -8,8 +8,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.Initializable;   
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.SwipeEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 
 /**
  *
@@ -17,18 +30,71 @@ import javafx.scene.control.Label;
  */
 public class FXMLheroesController implements Initializable {
     
-    @FXML
-    private Label label;
     
+ @FXML
+    private ScrollPane ScrollPane1;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private AnchorPane AnchorPane1;
+
+    @FXML
+    private Label Label1;
+
+    @FXML
+    private MenuButton FLASH;
+
+    @FXML
+    private MenuButton PUNISHER;
+
+    @FXML
+    private MenuButton NOVA;
+
+    @FXML
+    private MenuButton SONSPONT;
+
+    @FXML
+    private MenuButton SHAZAM;
+
+    @FXML
+    private MenuButton NIGHTCORE;
+
+    @FXML
+    private MenuButton MAGIK;
+
+    @FXML
+    private MenuButton DOMINO;
+
+    @FXML
+    private MenuButton WONDERWOMAN;
+
+    @FXML
+    private MenuButton AQUAMAN;
+
+    @FXML
+    private Label Label2;
+
+    @FXML
+    private Pane paneView;
+
+    @FXML
+    private TextField Txtbusqueda;
+
+    @FXML
+    private void displayImage(ActionEvent event) {
+        paneView.getChildren().clear();
+        String name=Txtbusqueda.getText();
+        Image image=new Image("/imagenes/"+name+".jpg");
+        javafx.scene.image.ImageView imageview=new javafx.scene.image.ImageView(image);
+        imageview.setFitWidth(200);
+        imageview.setFitHeight(300);
+        paneView.getChildren().add(imageview);
     }
     
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
 }
+    
